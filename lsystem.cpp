0637db8b4197b4300bcdbabbd66da06dcefe6f8b
Rohan Prinja <rohan.prinja@gmail.com>
2020-05-13 20:43:58 -0700

    add barnsley fern and add toggling between the two

--- lsystem.cpp
+++ lsystem.cpp
@@ -1,8 +1,9 @@
 // Build: g++ barnsley.cpp -lGL -lGLU -lglut
 // Run: ./lsystem
-// hit <esc> to exit
+// Hit <esc> to exit. B and F to toggle between Barnsley fern and Fractal tree.
 
 #include <cmath>
+#include <iostream>
 #include <sstream>
 #include <stack>
 #include <string>
@@ -12,8 +13,7 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-#define WINDOW_SIZE 900
-#define SEGMENT_LEN 0.005
+#define WINDOW_SIZE 1000
 
 using namespace std;
 
@@ -68,58 +68,112 @@ L_system barnsley_fern() {
 }
 
 // Coordinate system explanation: (0, 0) is the window centre. (1, 1) is the top right corner.
-void draw_fractal_tree(const string& draw_data) {
+void draw_fractal_tree() {
     glColor3f(0.f, 0.f, 0.f);
 
     point p = { 0.f, -0.9 };
+    const float k_delta_angle = M_PI / 4.0;
     float angle = M_PI / 2.0;
+    const double k_segment_len = 0.0075;
 
-    stack<pair<point, float>> s;
+    stack<pair<point, float>> state;
 
-    for (auto c : draw_data) {
+    for (auto c : g_draw_data) {
         if (c == '1' or c == '0') {
-            const point dp = { cos(angle) * SEGMENT_LEN, sin(angle) * SEGMENT_LEN };
+            const point dp = { cos(angle) * k_segment_len, sin(angle) * k_segment_len };
             glBegin(GL_LINES);
             glVertex2f(p.x, p.y);
             glVertex2f(p.x + dp.x, p.y + dp.y);
             glEnd();
             p += dp;
         } else if (c == '[') {
-            s.push(make_pair(p, angle));
-            angle -= M_PI / 4.0;
+            state.push(make_pair(p, angle));
+            angle -= k_delta_angle;
         } else if (c == ']') {
-            p = get<0>(s.top());
-            angle = get<1>(s.top()) + (M_PI / 4.0);
-            s.pop();
+            p = get<0>(state.top());
+            angle = get<1>(state.top()) + k_delta_angle;
+            state.pop();
         }
     }
 }
 
-// TODO
-void draw_barnsley_fern() {}
+void draw_barnsley_fern() {
+    glColor3f(0.f, 0.f, 0.f);
+
+    point p = { -0.7f, -0.9 };
+    const float k_delta_angle = 25 * M_PI / 180.0;
+    float angle = (M_PI / 2.0) - k_delta_angle;
+    const double k_segment_len = 0.0125f;
+
+    stack<pair<point, float>> state;
+
+    for (auto c : g_draw_data) {
+        if (c == 'F') {
+            const point dp = { cos(angle) * k_segment_len, sin(angle) * k_segment_len };
+            glBegin(GL_LINES);
+            glVertex2f(p.x, p.y);
+            glVertex2f(p.x + dp.x, p.y + dp.y);
+            glEnd();
+            p += dp;
+        } else if (c == '[') {
+            state.push(make_pair(p, angle));
+        } else if (c == ']') {
+            p = get<0>(state.top());
+            angle = get<1>(state.top());
+            state.pop();
+        } else if (c == '+') {
+            angle += k_delta_angle;
+        } else if (c == '-') {
+            angle -= k_delta_angle;
+        }
+    }
+}
+
+char g_active_fractal;
+L_system fractal_t;
+L_system barnsley;
 
 void display() {
     glClearColor(1.f, 1.f, 1.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
-    draw_fractal_tree(g_draw_data);
+    if (g_active_fractal == 'f') {
+        g_draw_data = fractal_t.contents;
+        draw_fractal_tree();
+    } else if (g_active_fractal == 'b') {
+        g_draw_data = barnsley.contents;
+        draw_barnsley_fern();
+    }
     glFlush();
 }
 
 void keyboard(unsigned char key, int x, int y) {
     if (key == 27) {
         glutDestroyWindow(g_window);
+    } else if (key == 'b') {
+        g_active_fractal = 'b';
+        glutPostRedisplay();
+    } else if (key == 'f') {
+        g_active_fractal = 'f';
+        glutPostRedisplay();
     }
 }
 
-int main(int argc, char** argv) {
-    L_system fractal_t = fractal_tree();
-    fractal_t.iterate(8);
-    g_draw_data = fractal_t.contents;
+void init() {
+    g_active_fractal = 'f';
 
+    fractal_t = fractal_tree();
+    fractal_t.iterate(8);
+
+    barnsley = barnsley_fern();
+    barnsley.iterate(6);
+}
+
+int main(int argc, char** argv) {
+    init();
     glutInit(&argc, argv);
     glutInitWindowSize(WINDOW_SIZE, WINDOW_SIZE);
     glutInitWindowPosition(400, 0);
-    g_window = glutCreateWindow("Barnsley");
+    g_window = glutCreateWindow("L-systems");
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMainLoop();
